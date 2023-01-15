18a623d773b8d0366b06e6fc2ec3e090ffb9d318
Jeremy Jacobson <29494813+xxk-i@users.noreply.github.com>
2023-01-15 03:51:18 -0500

    More CLI options

--- nier-fs-cli/src/main.rs
+++ nier-fs-cli/src/main.rs
@@ -1,13 +1,35 @@
 use clap::Parser;
+use clap::Subcommand;
 use std::fs::File;
 use nier_fs::*;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = "A CLI for working with NieR: Automata files")]
 struct Config {
-    // Path to file
+    // Pack or unpack
+    // Note: this is optional and default behavior is:
+    //  - pack folder
+    //  - unpack file
+    /// hi
+    #[command(subcommand)]
+    action: Option<Action>,
+    
+    /// Path to file or directory
     #[arg(short, long)]
     path: String,
+
+    /// Recurse subdirectories
+    #[arg(short, long)]
+    recursive: bool,
+}
+
+#[derive(Subcommand, Debug)]
+enum Action {
+    /// Pack directory into container (dir -> CPK/DAT)
+    Pack,
+
+    /// Unpack container into directory (CPK/DAT -> dir)
+    Unpack
 }
 
 fn main() {
